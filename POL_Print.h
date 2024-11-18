63947ecea78ed55df165bf04e300b8e36dffff15
GpapPeaky <giorgospapamat@gmail.com>
2024-11-19 01:26:28 +0200

    Added basic primitives, and overloads for signed and unsigned integers as well as pointers and booleans

---
+++ POL_Print.h
@@ -0,0 +1,9 @@
+#ifndef _POL_PRINT_H_
+/**
+ * @brief PeakyOverload printing macros and functions
+ */
+#define _POL_PRINT_H_
+
+#define POL_SizeOfTypePrint(type) printf("PPOL: size of " #type ": %zu bytes\n", sizeof(type))
+
+#endif
---
+++ main.c
@@ -0,0 +1,12 @@
+#include "POL.h"
+
+Uint32 main(void){
+
+    POL_SizeOfTypePrint(Sint64);
+    POL_SizeOfTypePrint(Sint32);
+    POL_SizeOfTypePrint(Sint16);
+    POL_SizeOfTypePrint(Sint8);
+    POL_SizeOfTypePrint(Reference);
+
+    return 0;
+}
